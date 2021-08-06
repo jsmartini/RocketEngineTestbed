ee4c054ccee25e8c62ea3a034fd5180b3619969b
Jonathan Martini <jsmartini@crimsoin.ua.edu>
2021-08-06 15:19:45 -0500

    fixed hardware main.py script

--- Hardware/main.py
+++ Hardware/main.py
@@ -12,53 +12,60 @@ from pressurecontrolsub import *
 Configure to same execution layout as testmain.py
 
 """
-async def main():
-    """
-    
-    Initializes ZMQ servers and the pressure controller sub
-    
 
-    TODO:
-
-        Split Loadcell, {System, remoteCLI}, and Pressure utilities into own threads and incorporate logging across all nodes
-    """
-
+def blocking_async(thread_loop):
     global CONFIG
-
-    asyncio.gather(
-        *[
-            PressureDataPub(
-                node_name='Pressure Data Aquisition',
-                update_tick=0.1, # a tenth of a second latency
-                port=CONFIG.DataConfig.PRESSURE_DATAFEED_PORT,
-                calibration = CONFIG.PressCalibration,
-                sensor_tuples=[[k,*CONFIG.PressureADCConfig[k]] for k in CONFIG.PressureADCConfig]
-            ),
-            remoteCLI(
+    asyncio.set_event_loop(thread_loop)
+    remoteCLICoro = remoteCLI(
                 node_name = "Remote Command Parser",
                 update_tick=0.1,
-                port = CONFIG.DataConfig.CMD_PROMPT_PORT
+                port = CONFIG["DataConfig"]["CMD_PROMPT_PORT"]
+            )
+    print("Running RemoteCLI Daemon")
+    thread_loop.run_until_complete(remoteCLICoro)    
+
+def non_blocking_async(thread_loop):
+    global CONFIG
+    asyncio.set_event_loop(thread_loop)
+    thread_loop.run_until_complete(asyncio.gather(
+            *[
+                SystemStatePub(
+                node_name = "System State Reporter",
+                update_tick=0.1,
+                port = CONFIG["DataConfig"]["SYSTEM_STATE_PORT"]
+            ),
+            PressureControllerSub(
+                node_name = "Pressure PID Controllers",
+                update_tick=0.1,
+                port = CONFIG["DataConfig"]["PRESSURE_DATAFEED_PORT"]
             ),
             LoadCellPub(
                 node_name="Load Cell Sensor Data Aquisition",
                 update_tick=0.01,
                 cell_name = "Load Sensor 1",
-                port = CONFIG.DataConfig.LOAD_CELL_DATAFEED_PORT,
+                port = CONFIG["DataConfig"]["LOAD_CELL_DATAFEED_PORT"]
             ),
-            SystemStatePub(
-                node_name = "System State Reporter",
-                update_tick=0.1,
-                port = CONFIG.DataConfig.SYSTEM_STATE_PORT
-            ),
-            PressureControllerSub(
-                node_name = "Pressure PID Controllers",
-                update_tick=0.1,
-                port = CONFIG.DataConfig.PRESSURE_DATAFEED_PORT
+            PressureDataPub(
+                node_name='Pressure Data Aquisition',
+                update_tick=0.1, # a tenth of a second latency
+                port=CONFIG["DataConfig"]["PRESSURE_DATAFEED_PORT"],
+                calibration = CONFIG["PressCalibration"],
+                sensor_tuples=[[k,*CONFIG["PressureADCConfig"][k]] for k in CONFIG["PressureADCConfig"]]
             )
-        ]
+            ]
+        )
     )
 
-loop = asyncio.get_event_loop()
-loop.run_forever(main())
+non_blocking_loop = asyncio.new_event_loop()
+blocking_loop = asyncio.new_event_loop()
+import threading
+threading.Thread(target=blocking_async, args=(blocking_loop,), name="RemoteCLI Daemon").start()
+threading.Thread(target=non_blocking_async, args=(non_blocking_loop,), name="RemoteCLI Daemon").start()
+
+
+
+
+
+
 
 
